d3182dcadb044df578306fa221339787e41f29ee
lucosmo <lukasz.kosmaczewski@gmail.com>
2022-01-03 21:45:39 +0000

    Update script.js
    Improved readability

--- script.js
+++ script.js
@@ -11,15 +11,12 @@ var price2=document.getElementById('price2').value;
 var quantity1=document.getElementById('quantity1').value;
 var quantity2=document.getElementById('quantity2').value;
 //alert("price1="+price1+"&price2="+price2+"&quantity1="+quantity1+"&quantity2="+quantity2);
-xmlhttp.onreadystatechange=function()
-  {
-  if (xmlhttp.readyState==4 && xmlhttp.status==200)
-    {
-	var content="<h2>"+xmlhttp.responseText+"</h2>";
+xmlhttp.onreadystatechange=function() {
+ if (xmlhttp.readyState==4 && xmlhttp.status==200) {
+    var content="<h2>"+xmlhttp.responseText+"</h2>";
     document.getElementById("answer").innerHTML=content;
-	
-    }
-  }
+ }
+}
 xmlhttp.open("GET","calculate.php?price1="+price1+"&price2="+price2+"&quantity1="+quantity1+"&quantity2="+quantity2,true);
 xmlhttp.send();
 return false;
